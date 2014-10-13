286493504eae5e0704118b8f145e370f48ce2d45
akrherz <akrherz@iastate.edu>
2014-10-13 13:00:53 -0500

    adjust the cron script to be easier to maintain

---
+++ scripts/RUN_FROST.sh
@@ -0,0 +1,7 @@
+# Do what's necessary for the Frost Model!
+export MM5I=/mesonet/www/apps/ISUMM5
+cd $MM5I
+python scripts/run_realtime.py $(date -u +'%Y %m %d') $1
+cd /mesonet/www/apps/bridget
+python scripts/run_bridget.py $MM5I/Run/isumm5_$(date -u +'%Y%m%d'){$1}00.nc 
+python scripts/make_plots.py output/$(date -u +'%Y%m%d'){$1}00_iaoutput.nc
