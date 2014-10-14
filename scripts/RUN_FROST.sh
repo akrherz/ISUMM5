0bf9bfc7f77b69ac2bb4b5d42fd59c685feb9020
akrherz <akrherz@iastate.edu>
2014-10-14 13:54:18 -0500

    wrong syntax for $1

--- scripts/RUN_FROST.sh
+++ scripts/RUN_FROST.sh
@@ -3,5 +3,5 @@ export MM5I=/mesonet/www/apps/ISUMM5
 cd $MM5I
 python scripts/run_realtime.py $(date -u +'%Y %m %d') $1
 cd /mesonet/www/apps/bridget
-python scripts/run_bridget.py $MM5I/MM5/Run/isumm5_$(date -u +'%Y%m%d'){$1}00.nc 
+python scripts/run_bridget.py $MM5I/MM5/Run/isumm5_$(date -u +'%Y%m%d')${1}00.nc 
 python scripts/make_plots.py output/$(date -u +'%Y%m%d'){$1}00_iaoutput.nc
