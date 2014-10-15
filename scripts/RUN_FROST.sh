8a37a73962e96b58e7703cadc3021c38ef488d39
akrherz <akrherz@iastate.edu>
2014-10-15 08:54:37 -0500

    oops, fix $1 for make_plots.py as well

--- scripts/RUN_FROST.sh
+++ scripts/RUN_FROST.sh
@@ -4,4 +4,4 @@ cd $MM5I
 python scripts/run_realtime.py $(date -u +'%Y %m %d') $1
 cd /mesonet/www/apps/bridget
 python scripts/run_bridget.py $MM5I/MM5/Run/isumm5_$(date -u +'%Y%m%d')${1}00.nc 
-python scripts/make_plots.py output/$(date -u +'%Y%m%d'){$1}00_iaoutput.nc
+python scripts/make_plots.py output/$(date -u +'%Y%m%d')${1}00_iaoutput.nc
